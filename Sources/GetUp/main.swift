60420e6fe1b08cd5b6666df1ed6df011150805ab
Brandon Camerer <v-brcame@microsoft.com>
2025-12-15 13:41:20 -0800

    Prevent multiple instances of the application from running

--- Sources/GetUp/main.swift
+++ Sources/GetUp/main.swift
@@ -1,5 +1,17 @@
 import Cocoa
 
+// Check for existing instances
+let bundleIdentifier = "com.getup.reminder.app"
+let runningApps = NSRunningApplication.runningApplications(withBundleIdentifier: bundleIdentifier)
+let currentPid = ProcessInfo.processInfo.processIdentifier
+
+for app in runningApps {
+    if app.processIdentifier != currentPid {
+        print("Another instance of GetUp is already running (PID: \(app.processIdentifier)). Terminating this instance.")
+        exit(0)
+    }
+}
+
 // Create the application instance
 let app = NSApplication.shared
 let delegate = AppDelegate()
